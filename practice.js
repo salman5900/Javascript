8ddb6c457add259794a6467e6fc1682a4f2d937d
salman5900 <salmanahamed5900@gmail.com>
2023-08-25 17:17:39 +0530

    new change learning functions 1

---
+++ practice.js
@@ -0,0 +1,13 @@
+function names(name,age) {
+    if(age >= 18) {
+        console.log("Damm "+ name + " you are an adult");
+    
+    }else {
+        console.log("Sorry "+ name +" you are not an adult");
+    }
+}
+
+names("salman",18);
+names("hana",13);
+names("zayan",5);
+names("nada",12);
