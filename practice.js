91f8db88a0bded899e2f2cb67e6f9542e0992b61
salman5900 <salmanahamed5900@gmail.com>
2023-08-25 18:00:44 +0530

    new change

--- practice.js
+++ practice.js
@@ -1,13 +1,41 @@
-function names(name,age) {
-    if(age >= 18) {
-        console.log("Damm "+ name + " you are an adult");
+// function names(name,age) {
+//     if(age >= 18) {
+//         console.log("Damm "+ name + " you are an adult");
     
-    }else {
-        console.log("Sorry "+ name +" you are not an adult");
-    }
+//     }else {
+//         console.log("Sorry "+ name +" you are not an adult");
+//     }
+// }
+
+// names("salman",18);
+// names("hana",13);
+// names("zayan",5);
+// names("nada",12);
+
+//=================================================
+
+// function whatShallWear(temp) {
+//     if (temp < 60) {
+//         console.log("Wear a jacket");
+//     }
+//     else if (temp < 70){
+//         console.log("Wear a sweater");
+//     }
+//     else {
+//         console.log("Wear T-shirt");
+//     }
+// }
+
+// whatShallWear(60);
+// whatShallWear(70);
+// whatShallWear(40);
+
+//-------------------------------------------
+
+function doIt(param) {
+    param = 2;
 }
 
-names("salman",18);
-names("hana",13);
-names("zayan",5);
-names("nada",12);
+var test = 1;
+doIt(test);
+console.log(test);
